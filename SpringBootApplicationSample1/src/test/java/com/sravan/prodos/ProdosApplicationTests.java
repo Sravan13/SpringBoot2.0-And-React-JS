0f04cf547476d7a0ebde72fbabcff37ed1c34fdf
Sravan13 <chithari.sravan@gmail.com>
2019-08-04 22:42:38 +0530

    Basic SpingBoot 2.x Application

---
+++ SpringBootApplicationSample1/src/test/java/com/sravan/prodos/ProdosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sravan.prodos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
