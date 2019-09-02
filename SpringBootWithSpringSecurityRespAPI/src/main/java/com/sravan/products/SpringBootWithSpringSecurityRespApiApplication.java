1bd27d7020abd1d540e68d3962651e1e641fc377
Sravan13 <chithari.sravan@gmail.com>
2019-09-02 21:59:21 +0530

    Add files via upload

---
+++ SpringBootWithSpringSecurityRespAPI/src/main/java/com/sravan/products/SpringBootWithSpringSecurityRespApiApplication.java
@@ -0,0 +1,13 @@
+package com.sravan.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithSpringSecurityRespApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithSpringSecurityRespApiApplication.class, args);
+	}
+
+}
